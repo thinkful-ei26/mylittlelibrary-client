75ad582a1c228bf3d7afb7234fc7b60beb5ba31a
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-05 12:48:52 -0700

    add-view to conosle.log success

--- src/components/add-view.js
+++ src/components/add-view.js
@@ -22,31 +22,30 @@ export class AddView extends React.Component {
             <p>
               <label htmlFor="Title">Title:</label>
               <Field name="title" id="title" type="text" component="input"/>
-              <input type="text" />
+     
             </p>
             <p>
               <label htmlFor="Author">Author:</label>
               <Field name="author" id="author" type="text" component="input"/>
-              <input type="text" />
+     
             </p>
             <p>
               <label htmlFor="Genre">Genre:</label>
               <Field name="genre" id="genre" type="text" component="input"/>
-              <input type="text" />
+     
             </p>
             <p>
               <label htmlFor="isbn">ISBN:</label>
               <Field name="isbn" id="isbn" type="text" component="input"/>
-              <input type="text" />
+     
             </p>
             <p>
-              <button>
-                <a href="addbook.html">Add</a>
+              <button type="submit">
+                Add
               </button>
             </p>
           </form>
         </fieldset>
-        <p>UNDER CONSTRUCTION.</p>
       </div>
     );
   }
