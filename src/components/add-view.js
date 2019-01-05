4be1031cf3c3fa252631754bde20cb9e9253b7d4
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-05 12:41:34 -0700

    add Fields to add-view

--- src/components/add-view.js
+++ src/components/add-view.js
@@ -21,18 +21,22 @@ export class AddView extends React.Component {
           >
             <p>
               <label htmlFor="Title">Title:</label>
+              <Field name="title" id="title" type="text" component="input"/>
               <input type="text" />
             </p>
             <p>
               <label htmlFor="Author">Author:</label>
+              <Field name="author" id="author" type="text" component="input"/>
               <input type="text" />
             </p>
             <p>
               <label htmlFor="Genre">Genre:</label>
+              <Field name="genre" id="genre" type="text" component="input"/>
               <input type="text" />
             </p>
             <p>
               <label htmlFor="isbn">ISBN:</label>
+              <Field name="isbn" id="isbn" type="text" component="input"/>
               <input type="text" />
             </p>
             <p>
