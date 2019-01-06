7d2198979b6c32a3f63aa311ac21160f2ef202e3
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-06 16:01:11 -0700

    refactor staff view

--- src/components/staff-view.js
+++ src/components/staff-view.js
@@ -1,48 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// This view will toggle between add and edit views
 export default function StaffView() {
   return (
     <div>
       <h2> Staff View - Will Require Login</h2>
-      <form action="">
-        <p>
-          <button>
-            <Link to={'./add-view'}>Add Books to catalog</Link>
-          </button>
-        </p>
-        <fieldset>
-          <legend>Search the catalog</legend>
-          <select name="" id="">
-            <option value="allFields">All Fields</option>
-            <option value="author">Author</option>
-            <option value="title">Title</option>
-            <option value="subject">Subject</option>
-          </select>
-          <input type="text" /> <button>Search</button>
-        </fieldset>
-      </form>
-      <section className="search-result" />
+
       <p>
-        UNDER CONSTRUCTION. This will be the interface for staff to perform CRUD
-        operations on db.
+        <button>
+          <Link to={'./add-view'}>Add Books to catalog</Link>
+        </button>
       </p>
-      <form action="">
-        <fieldset>
-          <legend>Sample Search Output</legend>
-          <ul className="title">
-            <li>Sample display title,</li>
-            <li>Sample display author</li>
-          </ul>
-          <button>
-            <Link to={'./edit-view'}>Edit</Link>
-          </button>
-          <button>
-           <Link to={'./delete-view'}> Delete</Link>
-          </button>
-        </fieldset>
-      </form>
+      <p>
+        <button>
+          <Link to={'./edit-view'}>Edit/Update existing books</Link>
+        </button>
+      </p>
+      <p>
+        <button>
+          <Link to={'./delete-view'}> Delete books from the catalog</Link>
+        </button>
+      </p>
     </div>
   );
 }
