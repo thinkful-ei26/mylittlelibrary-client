8ad084786e3d214a084bdb7ba0d3f11ecf32ede0
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-07 10:12:21 -0700

    additional validators for pw length, match and trim

--- src/validators.js
+++ src/validators.js
@@ -8,3 +8,20 @@ export const nonEmpty = value =>
 // @ A literal at sign
 // \S+ Matches one or more non-whitespace characters after the @
 // $ Matches the end of the text
+export const isTrimmed = value =>
+    value.trim() === value ? undefined : 'Cannot start or end with whitespace';
+
+export const length = length => value => {
+    if (length.min && value.length < length.min) {
+        return `Must be at least ${length.min} characters long`;
+    }
+    if (length.max && value.length > length.max) {
+        return `Must be at most ${length.max} characters long`;
+    }
+};
+
+
+export const matches = field => (value, allValues) =>
+    field in allValues && value.trim() === allValues[field].trim()
+        ? undefined
+        : 'Does not match';
