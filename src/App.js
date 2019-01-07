a39abbefadaab08cddfea24bdb0a69b8703df7e7
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-07 12:48:02 -0700

    reducers, store, actions built for authentication and connected, but registration form not yet rendered/operational

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Route, withRouter } from 'react-router-dom';
 import './App.css';
 import MainView from './components/main-view';
 import Header from './components/header';
@@ -8,11 +9,42 @@ import StaffView from './components/staff-view';
 import { EditViewInput } from './components/edit-view-input';
 import { AddViewInput } from './components/add-view-input';
 import { DeleteViewInput } from './components/delete-view-input';
+/*NEW REGISTRATION RELATED*/
+import Dashboard from './components/dashboard';
+import RegistrationPage from './components/registration-page';
+import { refreshAuthToken } from './actions/auth';
 
-class App extends React.Component {
+export class App extends React.Component {
+  componentDidUpdate(prevProps) {
+    if (!prevProps.loggedIn && this.props.loggedIn) {
+      // When we are logged in, refresh the auth token periodically
+      this.startPeriodicRefresh();
+    } else if (prevProps.loggedIn && !this.props.loggedIn) {
+      // Stop refreshing when we log out
+      this.stopPeriodicRefresh();
+    }
+  }
+  componentWillUnmount() {
+    this.stopPeriodicRefresh();
+  }
+
+  startPeriodicRefresh() {
+    this.refreshInterval = setInterval(
+      () => this.props.dispatch(refreshAuthToken()),
+      15 * 60 * 1000
+    );
+  }
+  stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+      return;
+    }
+
+    clearInterval(this.refreshInterval);
+  }
+
   render() {
     return (
-      <BrowserRouter>
+      
         <div>
           <Header />
           <Route exact path="/staff-view" component={StaffView} />
@@ -20,11 +52,17 @@ class App extends React.Component {
           <Route exact path="/edit-view" component={EditViewInput} />
           <Route exact path="/add-view" component={AddViewInput} />
           <Route exact path="/delete-view" component={DeleteViewInput} />
+          <Route exact path="/register" component={RegistrationPage} />
           <Footer />
         </div>
-      </BrowserRouter>
+      
     );
   }
 }
+const mapStateToProps = state => ({
+    hasAuthToken: state.auth.authToken !== null,
+    loggedIn: state.auth.currentUser !== null
+});
 
-export default App;
+// export default App;
+export default withRouter(connect(mapStateToProps)(App));
--- src/store.js
+++ src/store.js
@@ -1,20 +1,27 @@
-import {createStore, combineReducers} from 'redux'
-import {reducer as formReducer} from 'redux-form'
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form';
+import thunk from 'redux-thunk';
+import {loadAuthToken} from './local-storage';
+import authReducer from './reducers/auth';
+import protectedDataReducer from './reducers/protected-data';
+import {setAuthToken, refreshAuthToken} from './actions/auth';
 
-export default createStore(
-    combineReducers({
-        form: formReducer
-    })
-);
-
-/*
-redux method combineReducers lets us use the form reducer alongside other reducers without conflict
-If there were multiple reducers,
-export default createStore(
+const store = createStore(
     combineReducers({
         form: formReducer,
-        trello: trelloReducers
-    })
-)
+        auth: authReducer,
+        protectedData: protectedDataReducer
+    }),
+    applyMiddleware(thunk)
+);
 
-*/
+// Hydrate the authToken from localStorage if it exist
+const authToken = loadAuthToken();
+if (authToken) {
+    const token = authToken;
+    store.dispatch(setAuthToken(token));
+    store.dispatch(refreshAuthToken());
+}
+
+export default store;
+console.log('LOG', store)
