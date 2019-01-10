c726f2d236722574a4a240f38a1ccb189a9f9365
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-10 10:07:20 -0700

    testing

--- src/components/login-form.test.js
+++ src/components/login-form.test.js
@@ -6,8 +6,5 @@ describe('<LoginForm/>', () => {
   it('Renders without crashing', () => {
     shallow(<LoginForm />);
   });
-  it('Renders the log-in form', () => {
-    const wrapper = shallow(<LoginForm />);
-    expect(wrapper.some('.login-form')).toEqual(true)
-  });
+
 });
